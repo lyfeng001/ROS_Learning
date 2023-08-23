ef92f78cf7ba071ea12b063b9eaeffe70c7c3241
lyfeng001 <774033660@qq.com>
2023-08-23 22:19:16 +0800

    0823

--- src/plumbing_param_server/src/demo02_param_get.cpp
+++ src/plumbing_param_server/src/demo02_param_get.cpp
@@ -14,7 +14,29 @@ int main(int argc, char *argv[])
     //1.1
     double radius = nh.param("radius",0.6);
     ROS_INFO("param：radius=%.2f",radius);
+    //1.2
+    double radius2;
+    bool result = nh.getParam("radius",radius2);
+    if (result)
+    {
+        ROS_INFO("获取的半径是：%.2f",radius2);
+    }
+    else
+    {
+        ROS_INFO("被查询的的变量不存在"); 
+    }
+    //1.3
+    bool result2 = nh.getParamCached("radius",radius2);
+    if (result2)
+    {
+        ROS_INFO("获取的半径是：%.2f",radius2);
+    }
+    else
+    {
+        ROS_INFO("被查询的的变量不存在"); 
+    }
     //method 2 
+
 
     return 0;
 }
